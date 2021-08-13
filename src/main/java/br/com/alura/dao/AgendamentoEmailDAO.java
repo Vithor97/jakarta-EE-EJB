e9edcc955f8ab00d342ddd081b5d66624c7551f8
vithor97 <vitorsantos27@hotmail.com>
2021-08-12 23:06:07 -0300

    first commit

---
+++ src/main/java/br/com/alura/dao/AgendamentoEmailDAO.java
@@ -0,0 +1,22 @@
+package br.com.alura.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.alura.entidade.AgendamentoEmail;
+
+@Stateless
+public class AgendamentoEmailDAO {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public List<AgendamentoEmail> listar () {
+
+		return entityManager.createQuery("SELECT ae From AgendamentoEmail ae", AgendamentoEmail.class).getResultList();
+		
+	}
+}
