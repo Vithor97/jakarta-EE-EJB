59f24000049ceafad7aae2f9e5056edaeb5552c6
vithor97 <vitorsantos27@hotmail.com>
2021-08-17 20:03:21 -0300

    finalizacao jms

--- src/main/java/br/com/alura/job/AgendamentoEmailJob.java
+++ src/main/java/br/com/alura/job/AgendamentoEmailJob.java
@@ -2,9 +2,13 @@ package br.com.alura.job;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 
 import br.com.alura.entidade.AgendamentoEmail;
 import br.com.alura.servico.AgendamentoEmailServico;
@@ -15,6 +19,13 @@ public class AgendamentoEmailJob {
 	@Inject
 	private AgendamentoEmailServico agendamentoEmailServico;
 	
+	@Inject
+	@JMSConnectionFactory("java:jboss/DefaultJMSConnectionFactory")
+	private JMSContext context;
+	
+	@Resource(mappedName = "java:/jms/queue/EmailQueue")
+	private Queue queue;
+	
 	@Schedule(hour = "*", minute = "*", second = "*/10")
 	public void enviarEmail() {
 		
@@ -22,7 +33,7 @@ public class AgendamentoEmailJob {
 				agendamentoEmailServico.listarPorNaoAgendado();
 		
 		listarPorNaoAgendado.forEach(emailNaoAgendado -> {
-			agendamentoEmailServico.enviar(emailNaoAgendado);
+			context.createProducer().send(queue, emailNaoAgendado);
 			agendamentoEmailServico.alterar(emailNaoAgendado);
 		});
 	}
